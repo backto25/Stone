9eabc1d807321af191559496e8fbb76358563de1
kor-coeo <kor.coeo@gmail.com>
2019-04-08 10:06:47 +0800

    add computer and group model

---
+++ GroupModel.h
@@ -0,0 +1,52 @@
+/*  *****************************************************
+*****************************************************  */
+#ifndef GROUPMODEL_H
+#define GROUPMODEL_H
+
+#include <QVector>
+#include <QAbstractTableModel>
+#include <QString>
+
+#include "ComputerModel.h"
+#include "StaffModel.h"
+/*  *****************************************************
+*****************************************************  */
+class Group{
+  public:
+
+    int group_id;
+    int group_type;
+    QString group_name;
+
+    QVector<Staff> staffs;
+    QVector<Computer> computers;
+};
+/*  ************************************
+************************************  */
+class GroupModel : public QAbstractTableModel{
+    Q_OBJECT
+  private:
+    QVector<Group> groups;
+
+  public:
+    static const int DIM = 5;
+
+    GroupModel(QObject *parent = 0);
+    ~GroupModel();
+
+    int	rowCount(const QModelIndex & parent = QModelIndex()) const;
+    int	columnCount(const QModelIndex & parent = QModelIndex()) const;
+    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+
+    int findIndexById(int id);
+    QVector<int> findByType(int type);
+    Group getGroupByIndex(int index) const;
+    bool addOneGroup(Group group);
+    bool rmOneGroup(int index);
+    int size()const;
+
+    bool flashBySQL();
+};
+/*  ************************************
+************************************  */
+#endif
